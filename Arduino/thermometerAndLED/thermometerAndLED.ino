dc51a82437e913ad575cd95d5657f761729aa755
Adrian Beściak <adrian.besciak99@gmail.com>
2020-04-09 11:33:37 +0100

    added dev1 measuring temperature and switching LED

--- Arduino/thermometerAndLED/thermometerAndLED.ino
+++ Arduino/thermometerAndLED/thermometerAndLED.ino
@@ -1,17 +1,45 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+
+String name = "dev1";
+
+#define thermometerPin 2
+#define LEDPin 3
  
-OneWire oneWire(2); //Podłączenie do A5
+OneWire oneWire(thermometerPin); //Podłączenie do A5
 DallasTemperature sensors(&oneWire); //Przekazania informacji do biblioteki
  
 void setup(void) {
+  pinMode(LEDPin, OUTPUT);
+  digitalWrite(LEDPin, LOW);
   Serial.begin(9600);
   sensors.begin(); //Inicjalizacja czujnikow
 }
  
-void loop(void) { 
-  sensors.requestTemperatures(); //Pobranie temperatury czujnika
-  Serial.print("Aktualna temperatura: ");
-  Serial.println(sensors.getTempCByIndex(0));  //Wyswietlenie informacji
-  delay(2000);
+void loop(void) {
+
+  if ( Serial.available() > 0 )
+  {
+    String rec = Serial.readStringUntil('\n');
+    if ( rec == "getName" )
+      Serial.println(name);
+    else if ( rec == "getTemperature" )
+    {
+      sensors.requestTemperatures(); //Pobranie temperatury czujnika
+      Serial.println(sensors.getTempCByIndex(0));  //Wyswietlenie informacji
+    }
+    else if (rec == "turnOnLED")
+    {
+      digitalWrite(LEDPin, HIGH);
+      Serial.println("LEDTurnedOn");
+    }
+    else if (rec == "turnOffLED")
+    {
+      digitalWrite(LEDPin, LOW);
+      Serial.println("LEDTurnedOff");
+    }
+    else
+      Serial.println(rec);
+  }
+  
 }
