269075e355eae2592ed0ce83104f51c49ccce0c9
Adrian Beściak <adrian.besciak99@gmail.com>
2020-04-26 23:16:54 +0200

    modified Logger to use MongoCollection

--- system/logger.py
+++ system/logger.py
@@ -1,16 +1,14 @@
-from pymongo import MongoClient
 import datetime
+import mongoCollection
 
 
 class Logger:
     def __init__(self):
-        self.__client = MongoClient()
-        self.__db = self.__client.SmartHomeServer
-        self.__logs = self.__db.logs
+        self.__logs = mongoCollection.MongoCollection("logs")
 
     def addlog(self, device, service, message):
         log = { "device": device.getname(),
                 "service": service,
                 "date": datetime.datetime.utcnow(),
                 "message": message}
-        self.__logs.insert_one(log)
+        self.__logs.send(log)
