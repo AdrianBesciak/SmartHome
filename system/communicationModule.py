05c5fdab08f7c23d571c1e77c8877acf77160e38
Adrian Beściak <adrian.besciak99@gmail.com>
2020-04-29 17:09:11 +0200

    changed communicationModule.py to use dicts in pipe

--- system/communicationModule.py
+++ system/communicationModule.py
@@ -5,19 +5,21 @@ def main(pipe):
     devices_dict = {}
     while True:
         if pipe.poll(1):
-            rec = pipe.recv().split(':')
+            rec = pipe.recv()
             for i in rec:
-                print('\t' + i)
+                print('\tkey: ' + i + 'value: ' + rec[i])
             for i in devices_dict:
                 print('\t\t' + i)
-            if rec[0] == 'register_device':
+            if rec['command'] == 'register_device':
                 print('Register dev' + rec[1])
-                if rec[1] == 'serial':
-                    devices_dict[rec[2]] = serialdevice.SerialDevice(rec[2])
-            elif rec[0] == 'send2dev':
-                pipe.send(devices_dict[rec[1]].talk(rec[2]))
-            elif rec[0] == 'services':
-                pipe.send(devices_dict[rec[1]].getserviceslist())
+                if rec['dev_type'] == 'serial':
+                    devices_dict[rec['dev_name']] = serialdevice.SerialDevice(rec['dev_name'])
+            elif rec['dev_type'] == 'send2dev':
+                pipe.send(devices_dict[rec['dev_name']].talk(rec['message']))
+            elif rec['dev_type'] == 'services':
+                pipe.send(devices_dict[rec['dev_name']].getserviceslist())
+            elif rec['dev_type'] == 'devs':
+                pipe.send(devices_dict)
             else:
                 pipe.send('Unrecognized command')
 
