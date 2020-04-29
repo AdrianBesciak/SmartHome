354220d540148d37c059d655b0ed892fed0cd3be
Adrian Beściak <adrian.besciak99@gmail.com>
2020-04-29 16:59:39 +0200

    changed core.py to use dicts in pipe

--- system/core.py
+++ system/core.py
@@ -3,38 +3,22 @@ import communicationModule as cm
 
 
 def main():
-    print("Type name of the device in /dev/ divtionary")
-    devName = input()
+    print("Type name of the device in /dev/ dictionary")
+    dev_name = input()
     p_conn, c_conn = mp.Pipe()
     p = mp.Process(target=cm.main, args=(c_conn,))
     p.start()
-    p_conn.send('register_device:serial:' + devName)
-    '''    
-    print("SelfCheck!")
-    print("Getting the name of the Arduino module!")
-    p_conn.send("getname")
-    if p_conn.poll(10):
-        print(p_conn.recv())
-    else:
-        print("Arduino timed out.")
-        print("Counting to 9")
-    for i in range(10):
-        p_conn.send(i)
-        print(p_conn.recv())
+    p_conn.send({'command': 'register_device', 'dev_type': 'serial', 'dev_name': dev_name})
 
-    print("Check out the echo!")
-    print("Write your word, and then the number of repetitions.")
-    print("Write \"close\" to exit.")
-    '''
     while True:
         s = input()
         if s == "close":
             print("Wychodze")
             break
         if s == 'services':
-            p_conn.send(s + ':' + devName)
+            p_conn.send({'command': 'services', 'dev_name': dev_name})
         else:
-            p_conn.send('send2dev:' + devName + ':' + s)
+            p_conn.send({'command': 'send2dev', 'devName': dev_name, 'message': s})
         if p_conn.poll(10):
             print(p_conn.recv())
         else:
