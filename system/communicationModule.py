0a75a2fba95835d6ed04bf4ef1ea4e029678250e
Adrian Beściak <adrian.besciak99@gmail.com>
2020-04-29 14:07:55 +0200

    added communication via pipe in communicationModule.py

--- system/communicationModule.py
+++ system/communicationModule.py
@@ -1,11 +1,25 @@
 import serialdevice
 
 
-def main(com):
-    dev = serialdevice.SerialDevice('ttyACM0')
+def main(pipe):
+    devices_dict = {}
     while True:
-        if com.poll(1):
-            com.send(dev.talk(com.recv()))
+        if pipe.poll(1):
+            rec = pipe.recv().split(':')
+            for i in rec:
+                print('\t' + i)
+            for i in devices_dict:
+                print('\t\t' + i)
+            if rec[0] == 'register_device':
+                print('Register dev' + rec[1])
+                if rec[1] == 'serial':
+                    devices_dict[rec[2]] = serialdevice.SerialDevice(rec[2])
+            elif rec[0] == 'send2dev':
+                pipe.send(devices_dict[rec[1]].talk(rec[2]))
+            elif rec[0] == 'services':
+                pipe.send(devices_dict[rec[1]].getserviceslist())
+            else:
+                pipe.send('Unrecognized command')
 
 
 if __name__ == "__main__":
