bc101b69d5de02c217493929f1b3274d344153e6
Adrian Beściak <adrian.besciak99@gmail.com>
2020-04-29 14:16:41 +0200

    added typing name of the device on the beginning of core

--- system/core.py
+++ system/core.py
@@ -3,10 +3,12 @@ import communicationModule as cm
 
 
 def main():
+    print("Type name of the device in /dev/ divtionary")
+    devName = input()
     p_conn, c_conn = mp.Pipe()
     p = mp.Process(target=cm.main, args=(c_conn,))
     p.start()
-    p_conn.send('register_device:serial:ttyUSB1')
+    p_conn.send('register_device:serial:' + devName)
     '''    
     print("SelfCheck!")
     print("Getting the name of the Arduino module!")
@@ -30,9 +32,9 @@ def main():
             print("Wychodze")
             break
         if s == 'services':
-            p_conn.send(s + ':ttyUSB1')
+            p_conn.send(s + ':' + devName)
         else:
-            p_conn.send('send2dev:ttyUSB1:' + s)
+            p_conn.send('send2dev:' + devName + ':' + s)
         if p_conn.poll(10):
             print(p_conn.recv())
         else:
