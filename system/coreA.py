3043c8ddfd0756661349a524647eaf6e1a9ed516
Marcin <malcher@student.agh.edu.pl>
2020-04-30 12:43:48 +0200

    Adding devices

--- system/coreA.py
+++ system/coreA.py
@@ -34,9 +34,22 @@ def main():
                 usr = input("Which user?")
                 priv = input("Which privilege?")
                 login.revoke_privilege(usr, priv)
+        elif s == "add_device":
+            if login.check_privilege("add"):
+                dev = input("Device port? ")
+                p_conn.send({'command': 'register_device', 'dev_type': 'serial', 'dev_port': dev})
+            else:
+                print("You do not have access to this. Ask your admin.")
         elif s == 'services':
             if login.check_privilege("list"):
-                p_conn.send({'command': 'services', 'dev_name': dev_name})
+                dev = input("Which device?")
+                p_conn.send({'command': 'devs'})
+                devices = p_conn.recv()
+                if dev in devices and login.check_privilege("dev_"+dev):
+                    p_conn.send({'command': 'services', 'dev_name': dev})
+                else:
+                    print("The device does not exist or you do not have permission to touch it")
+                    print("Use <devices_list> to get the list of devices.")
             else:
                 print("You do not have access to this. Ask your admin.")
         elif s == 'devices_list':
@@ -47,12 +60,15 @@ def main():
                     print(i)
             else:
                 print("You do not have access to this. Ask your admin.")
-        elif s.startswith('send'):
+        elif s == "send":
             if login.check_privilege("send"):
-                tokens = s.split()
-                if len(tokens) >= 3:
-                    if login.check_privilege("dev_" + tokens[1]):
-                        p_conn.send({'command': 'send2dev', 'dev_name': tokens[1], 'message': tokens[2]})
+                dev = input("Device? ")
+                com = input("Service? ")
+                p_conn.send({'command': 'devs'})
+                devices = p_conn.recv()
+                if dev in devices:
+                    if login.check_privilege("dev_" + dev):
+                        p_conn.send({'command': 'send2dev', 'dev_name': dev, 'message': com})
                     else:
                         print("You do not have access to this device!")
                 else:
@@ -63,6 +79,7 @@ def main():
                 print("You do not have access to this. Ask your admin.")
         else:
             print("Available commands: close, login, logout, devices_list, services, send")
+            print("Admin commands: grant, revoke")
             """if login.check_privilege("send")
                 p_conn.send({'command': 'send2dev', 'dev_name': dev_name, 'message': s})"""
         if p_conn.poll(10):
