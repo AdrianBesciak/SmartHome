04da302e34b5de75cd3077c60b202ca1b247b643
Adrian Beściak <adrian.besciak99@gmail.com>
2020-05-12 19:56:19 +0200

    added not working logging

--- webapp/httpserver.py
+++ webapp/httpserver.py
@@ -1,7 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+
+
+def valid_login(username, password):
+    if username == 'Adrian' and password == 'kij':
+        return True
+    return False
+
+
+def log_user_in(username):
+    return app.route('login/username');
 
 
 @app.route('/')
@@ -9,9 +19,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['POST', 'GET'])
 def login():
-    return render_template('login.html')
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['pwd']):
+            #return log_user_in(request.form['username'])
+            print('Zalogowano')
+            return render_template(logged_in, username=request.form['username'])
+        else:
+            print('Nie Zalogowano')
+            error = 'Invalid username or password'
+    print("Nie wiem co sie dzieje")
+    return render_template('login.html', error=error)
 
 
 @app.route('/login/<name>')
