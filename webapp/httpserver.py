a824a18f602986ac1c7c1252547982d418389989
Adrian Beściak <adrian.besciak99@gmail.com>
2020-05-12 18:05:15 +0200

    added LogIn button to index.html

--- webapp/httpserver.py
+++ webapp/httpserver.py
@@ -8,8 +8,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
 @app.route('/login/<name>')
-def login(name):
+def logged_in(name):
     return render_template('login.html', name=name)
 
 
