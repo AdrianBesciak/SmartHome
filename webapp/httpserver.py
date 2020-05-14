d266f043222428a2d3fa0fa68699620fb69564b3
Adrian Beściak <adrian.besciak99@gmail.com>
2020-05-14 12:02:42 +0200

    modified httpserver, login and register pages to use new forms

--- webapp/httpserver.py
+++ webapp/httpserver.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'afaa73978854986497574dcae8357ba7'
 
 
 def valid_login(username, password):
@@ -19,30 +22,16 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['pwd']):
-            #return log_user_in(request.form['username'])
-            print('Zalogowano')
-            return render_template(logged_in, username=request.form['username'])
-        else:
-            print('Nie Zalogowano')
-            error = 'Invalid username or password'
-    print("Nie wiem co sie dzieje")
-    return render_template('login.html', error=error)
-
-
-@app.route('/login/<name>')
-def logged_in(name):
-    return render_template('loggedIn.html', name=name)
-
-
 @app.route('/register')
 def register():
-    return render_template('register.html')
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
